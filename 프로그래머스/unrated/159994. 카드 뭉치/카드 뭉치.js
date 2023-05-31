8e5d3ffc6681bb01b32952aaf11e6c1ffddfbe48
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-05-31 19:58:40 +0900

    [unrated] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,20 @@
+function solution(cards1, cards2, goal) {
+    let answer = 'Yes';
+    
+    for(let i=0; i<goal.length; i++) {
+        if(goal[i] === cards1[0]) {
+            cards1.shift();
+            continue;
+        }
+        else if(goal[i] === cards2[0]) {
+            cards2.shift();
+            continue;
+        }
+        else {
+            answer = 'No';
+            break;
+        }
+    }
+    
+    return answer;
+}
