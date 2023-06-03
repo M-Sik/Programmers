fea948a80a85834b2e09d1e27b2293771f7e36fa
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-06-03 18:07:18 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 4.05 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,27 +1,29 @@
 function solution(board, moves) {
     let answer = 0;
-    let stacks = [];
+    let stack = [];
     
     moves.forEach((delNum) => {
         for(let i=0; i<board.length; i++) {
             // 기계가 뽑을 라인에 있는 수가 0이 아니라면 뽑은 후 0으로 변경
             if(board[i][delNum - 1] !== 0) {
-                stacks.push(board[i][delNum - 1]);
+                stack.push(board[i][delNum - 1]);
                 board[i][delNum - 1] = 0;
                 // 하나를 뽑은 후 아래 인형을 뽑지 않기위해 멈춤
                 break;
             }
         }
-        if(stacks.length !== 1) {
-            for(let i=0; i<stacks.length; i++) {
-                if(stacks[i] === stacks[i+1]) {
-                    stacks.splice(i, 2);
+        // 인형을 하나씩 뽑을때마다 뽑은 인행들의 배열에 2개이상의 인형이 있다면 사라질 인형이 있는지 확인
+        if(stack.length !== 1) {
+            // 뽑은 인형들을 탐색한 후
+            for(let i=0; i<stack.length; i++) {
+                // 2개의 같은 인형이 있다면 뽑은 인형 배열에서 제거 후 사라진 인형수 2개 추가
+                if(stack[i] === stack[i+1]) {
+                    stack.splice(i, 2);
                     answer += 2;
                 }
             }
         }
     })
-    // console.log(stacks)
     
     return answer;
 }
