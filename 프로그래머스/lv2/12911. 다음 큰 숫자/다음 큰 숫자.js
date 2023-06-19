36b7c19dc071604a8a2bdd3d05592408ffae8be3
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-06-19 11:12:38 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,20 +1,21 @@
 function solution(n) {
     let answer = 0;
-    const initNOneCount = [...n.toString(2)].reduce((acc, cur) => {
-        if(cur === "1") return acc + parseInt(cur);
-        return acc + 0;
-    }, 0);
+    const initNOneCount = binaryConversion(n);
     let nextNOneCount = 0;
     
     while(answer === 0) {
         n++;
-        nextNOneCount = [...n.toString(2)].reduce((acc, cur) => {
-            if(cur === "1") return acc + parseInt(cur);
-            return acc + 0;
-        }, 0);
+        nextNOneCount = binaryConversion(n);
         
         if(initNOneCount === nextNOneCount) answer = n;
     }
     
     return answer;
+}
+
+function binaryConversion (number) {
+   return [...number.toString(2)].reduce((acc, cur) => {
+        if(cur === "1") return acc + parseInt(cur);
+        return acc + 0;
+    }, 0);
 }
