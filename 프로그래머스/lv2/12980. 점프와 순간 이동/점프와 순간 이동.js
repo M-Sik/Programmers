611955e6efe4d03c1de1523fc83b2b1f2cc89a66
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-07-01 15:11:45 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,15 @@
+function solution(n)
+{
+    let ans = 0;
+
+    while(n > 0) {
+        if(n % 2 !== 0) {
+            n -= 1;
+            ans += 1;
+        }
+        
+        n = n / 2;
+    }
+
+    return ans;
+}
