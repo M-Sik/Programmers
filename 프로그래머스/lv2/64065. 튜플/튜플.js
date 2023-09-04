563c0923e00080beb845e5be1149be7cc05cc377
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-09-04 18:44:32 +0900

    [level 2] Title: 튜플, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -2,10 +2,8 @@ function solution(s) {
     let answer
     let obj = {};
     
-    s = s.replaceAll("{", '[');
-    s = s.replaceAll("}", ']');
+    s = s.replaceAll("{", '[').replaceAll("}", ']');
     s = JSON.parse(s);
-    // console.log(s);
     
     s.forEach((v) => {
         v.forEach((k) => {
@@ -13,7 +11,6 @@ function solution(s) {
         })
     })
     answer = Object.entries(obj).sort((a, b) => b[1] - a[1]).map(([num, _]) => parseInt(num));
-    // console.log(answer)
     
     return answer;
 }
