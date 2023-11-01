60bb277d944f0734b461e2381ed25562f4715930
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-11-01 20:18:01 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,6 @@
+function solution(my_string, overwrite_string, s) {
+    const arr = my_string.split("");
+    arr.splice(s, overwrite_string.length, overwrite_string);
+
+    return arr.join("");
+}
