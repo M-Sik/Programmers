5b4af2bf8676df20b893779cf100504011f46d67
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-12-13 17:25:19 +0900

    [level unrated] Title: 배열 만들기 3, Time: 0.34 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,11 @@
+function solution(arr, intervals) {
+    let answer = [];
+    
+    intervals.forEach(([a, b]) => {
+        for(let i=a; i<=b; i++) {
+            answer.push(arr[i]);
+        }
+    })
+    
+    return answer;
+}
