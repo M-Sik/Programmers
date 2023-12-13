32cd642f06db8cb56dd087ed5bd7cfd23b7ee0cd
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-12-13 17:41:10 +0900

    [level unrated] Title: 2의 영역, Time: 0.35 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
@@ -1,13 +1,6 @@
 function solution(arr) {
-    let answer = [];
-    let indexs = [];
-    
-    arr.forEach((v, i) => {
-        v === 2 && indexs.push(i);
-    })
-    for(let i=indexs[0]; i<=indexs[indexs.length-1]; i++) {
-        answer.push(arr[i]);
-    }
-    
-    return answer.length === 0 ? [-1] : answer;
+    const from = arr.indexOf(2);
+    const end = arr.lastIndexOf(2);
+
+    return from === -1 ? [-1] : arr.slice(from, end+1);
 }
