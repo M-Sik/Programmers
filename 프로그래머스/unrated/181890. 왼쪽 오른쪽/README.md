e787d102eb889cb6d7fa1a54e586bfa4fdee996b
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2023-12-14 16:34:36 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/README.md
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 왼쪽 오른쪽 - 181890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.04 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 16:32:26
+2023년 12월 4일 16:34:35
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,16 +1,11 @@
 function solution(str_list) {
-    let answer = [];
-    
     for(let i=0; i<str_list.length; i++) {
         if(str_list[i] === 'l') {
-            answer.push(...str_list.slice(0, i));
-            break;
+            return str_list.slice(0, i);
         }
-        else if(str_list[i] === 'r') {
-            answer.push(...str_list.slice(i+1));
-            break;
+        if(str_list[i] === 'r') {
+            return str_list.slice(i+1);
         }
     }
-    
-    return answer;
+    return [];
 }
