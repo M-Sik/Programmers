c317d00143c2a127011e09193fe8868ce3d98650
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2024-02-04 15:35:33 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,7 @@
+function solution(binomial) {
+    const arr = binomial.split(" ");
+    
+    if(arr[1] === '+') return Number(arr[0]) + Number(arr[2]);
+    else if(arr[1] === '-') return Number(arr[0]) - Number(arr[2]);
+    else return Number(arr[0]) * Number(arr[2]);
+}
