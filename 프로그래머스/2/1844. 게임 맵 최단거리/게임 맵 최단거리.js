590350ac448dc74f8f7f8fd52a9ba2444689bdb6
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2024-02-22 16:42:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.58 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -2,8 +2,10 @@ function solution(maps) {
     const n = maps[0].length - 1;
     const m = maps.length - 1;
     const queue = [[0, 0, 1]];
+    let i = 0;
     
     while(queue.length) {
+        i++;
         const [x,y,move] = queue.shift();
         // 최종 목적지 도달시 움직인 수 리턴
         if (x == n && y == m) return move;
@@ -15,6 +17,9 @@ function solution(maps) {
         if (x+1 <= n && maps[y][x+1]) queue.push([x+1,y,move+1]); // 오른쪽으로 한칸 이동
         if (y-1 >= 0 && maps[y-1][x]) queue.push([x,y-1,move+1]); // 아래로 한칸 이동
         if (y+1 <= m && maps[y+1][x]) queue.push([x,y+1,move+1]); // 위로 한칸 이동
+        
+        
+        // console.log(queue, i)
     }
     return -1;
 }
