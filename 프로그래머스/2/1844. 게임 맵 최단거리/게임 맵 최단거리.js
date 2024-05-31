171ec230cb54298ede6e684fbb9715b8565c317f
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2024-06-01 02:18:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 28.54 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,25 +1,34 @@
+// 출발지, 목적지 설정
+// bfs 큐에 있는걸 먼저 탐색 -> 가장 빨리 도착한것 선택
+// 큐가 비어있을때 까지 상하좌우 이동할 수 있는곳 있는지 탐색, 이동할 수 있다면 이동 후 이동표시
 function solution(maps) {
-    const n = maps[0].length - 1;
-    const m = maps.length - 1;
+    const n = maps.length -1;
+    const m = maps[0].length -1;
     const queue = [[0, 0, 1]];
-    let i = 0;
+    let nextRow = 0;
+    let nextCol = 0;
+    const dRow = [0, 1, 0, -1];
+    const dCol = [1, 0, -1, 0];
     
-    while(queue.length) {
-        i++;
-        const [x,y,move] = queue.shift();
-        // 최종 목적지 도달시 움직인 수 리턴
-        if (x == n && y == m) return move;
+    while(queue.length !== 0) {
+        const [curRow, curCol, distance] = queue.shift();
+        // 목적지 도달시 리턴
+        if(curRow === n && curCol === m) return distance;
         
-        if (!maps[y][x]) continue;
-        maps[y][x] = 0;
-
-        if (x-1 >= 0 && maps[y][x-1]) queue.push([x-1,y,move+1]); // 왼쪽으로 한칸 이동
-        if (x+1 <= n && maps[y][x+1]) queue.push([x+1,y,move+1]); // 오른쪽으로 한칸 이동
-        if (y-1 >= 0 && maps[y-1][x]) queue.push([x,y-1,move+1]); // 아래로 한칸 이동
-        if (y+1 <= m && maps[y+1][x]) queue.push([x,y+1,move+1]); // 위로 한칸 이동
-        
-        
-        // console.log(queue, i)
+        for(let i=0; i<4; i++) {
+            nextRow = curRow + dRow[i];
+            nextCol = curCol + dCol[i];
+            // 지도밖에 안나가게
+            if(0<=nextRow && nextRow<=n && 0<=nextCol && nextCol<=m) {
+                // console.log(nx, ny);
+                // 움직일 좌표가 벽이 아니라면
+                if(maps[nextRow][nextCol] === 1) {
+                    queue.push([nextRow, nextCol, distance + 1]);
+                    maps[nextRow][nextCol] = 0;
+                }
+            }
+        }
     }
+    
     return -1;
 }
