2c822952ba95ff3573ff27020dd00ccd54cc736c
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2024-05-31 18:34:35 +0900

    [level 2] Title: 피로도, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,18 +1,19 @@
 function solution(k, dungeons) {
-    let answer = 0;
+    const arr = [];
     const visited = new Array(dungeons.length).fill(false);
-    
-    const dfs = (남은피로도, level) => {
-        for(let i=0; i<visited.length; i++) {
-            if(!visited[i] && 남은피로도 >= dungeons[i][0]) {
+    const dfs = (hp, level) => {
+        for(let i=0; i<dungeons.length; i++) {
+            if(!visited[i] && hp >= dungeons[i][0]) {
                 visited[i] = true;
-                dfs(남은피로도 - dungeons[i][1], level + 1);
+                dfs(hp - dungeons[i][1], level + 1);
                 visited[i] = false;
             }
         }
-        answer = Math.max(answer, level);
+        arr.push(level);
     }
-    dfs(k, 0);
     
-    return answer;
+    dfs(k, 0);
+    const answer = [...new Set(arr)];
+    
+    return Math.max(...answer);
 }
