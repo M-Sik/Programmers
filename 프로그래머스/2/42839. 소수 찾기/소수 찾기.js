766a4e2dd0f55506ae616e283b0fd38c14bd9f54
MyeongSik <77043973+M-Sik@users.noreply.github.com>
2024-05-31 18:55:30 +0900

    [level 2] Title: 소수 찾기, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,32 +1,27 @@
 const isPrime = (num) => {
-    if (num === 0 || num === 1) return false;
+    if(num === 0 | num === 1) return false;
     for(let i=2; i<=Math.sqrt(num); i++) {
         if(num % i === 0) return false;
     }
-    return true;
+    return true
 }
 
 function solution(numbers) {
-    let answer = 0;
-    const temp = [];
-    const strs = [...numbers];
+    let answer = [];
     const visited = new Array(numbers.length).fill(false);
     const dfs = (cur) => {
         for(let i=0; i<numbers.length; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                dfs(cur + strs[i]);
-                visited[i] = false;
-            }
+            if(visited[i]) continue;
+            
+            visited[i] = true;
+            dfs(cur + numbers[i]);
+            visited[i] = false;
         }
-        temp.push(+cur)
+        answer.push(+cur);
     }
     
-    dfs("")
-    const a = [...new Set(temp)];
-    a.forEach((item) => {
-        if(isPrime(item)) answer++;
-    })
+    dfs("");
+    answer = [...new Set(answer)].reduce((acc, cur) => isPrime(cur) ? acc += 1 : acc,0);
     
     return answer;
 }
